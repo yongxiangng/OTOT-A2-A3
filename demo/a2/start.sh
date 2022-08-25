4852eb22064d6d2db25f48a2f7beaa9d255733c6
yongxiangng <yongxiangng@gmail.com>
2022-08-25 23:47:48 +0800

    tidy

--- demo/a2/start.sh
+++ demo/a2/start.sh
@@ -3,37 +3,32 @@ echo running start script...
 echo creating clusters...
 kind create cluster --name kind-1 --config ../../k8s/kind/cluster-config.yaml
 
-kubectl cluster-info
-kubectl get nodes
+kubectl cluster-info --context kind-kind-1
+kubectl get nodes --context kind-kind-1
 
 echo creating deployment
-kubectl apply -f ../../k8s/manifests/k8s/backend-service.yaml
+kubectl apply -f ../../k8s/manifests/k8s/backend-service.yaml --context kind-kind-1
 
-kubectl wait --for=condition=ready deploy --selector=app=backend --timeout=180s
-
-kubectl get po -l app=backend -o wide
+kubectl wait --for=condition=ready pod --selector=app=backend --timeout=180s --context kind-kind-1
+kubectl get po -l app=backend -o wide --context kind-kind-1
 
 echo creating service...
-kubectl apply -f ../../k8s/manifests/k8s/service.yaml
+kubectl apply -f ../../k8s/manifests/k8s/service.yaml --context kind-kind-1
 
-kubectl wait --for=condition=ready service --selector=app=backend-service --timeout=90s
-
-kubectl describe svc backend
-kubectl get po -l app=backend-service -o wide
+kubectl describe svc backend-service --context kind-kind-1
+kubectl get svc -l app=backend -o wide --context kind-kind-1
 
 echo creating ingress controller...
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml --context kind-kind-1
 
-kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
-kubectl --namespace ingress-nginx get po -l app.kubernetes.io/component=controller -o wide
-
-kubectl -n ingress-nginx get deploy 
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s --context kind-kind-1
+kubectl --namespace ingress-nginx get po -l app.kubernetes.io/component=controller -o wide --context kind-kind-1
+kubectl -n ingress-nginx get deploy --context kind-kind-1
 
 echo creating ingress...
-kubectl apply -f ../../k8s/manifests/k8s/ingress.yaml
+kubectl apply -f ../../k8s/manifests/k8s/ingress.yaml --context kind-kind-1
 
-kubectl wait --for=condition=ready ingress --selector=app=backend-ingress --timeout=180s
-kubectl get po -l app=backend-ingress -o wide
+kubectl get ingress -l app=backend -o wide --context kind-kind-1
 
 sleep 10 # sneaky sleep, instantly curling has issues
 
