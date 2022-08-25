7e22cf73d0ef8eb7d9848b299000e34b11edea52
yongxiangng <yongxiangng@gmail.com>
2022-08-25 23:04:00 +0800

    log script

--- demo/a2/start.sh
+++ demo/a2/start.sh
@@ -19,18 +19,21 @@ kubectl apply -f ../../k8s/manifests/k8s/service.yaml
 kubectl wait --for=condition=ready service --selector=app=backend-service --timeout=90s
 
 kubectl describe svc backend
+kubectl get po -l app=backend-service -o wide
 
 echo creating ingress controller...
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
+kubectl --namespace ingress-nginx get po -l app.kubernetes.io/component=controller -o wide
 
 kubectl -n ingress-nginx get deploy 
 
 echo creating ingress...
 kubectl apply -f ../../k8s/manifests/k8s/ingress.yaml
 
-kubectl wait --for=condition=ready ingress --selector=app=backend-ingress --timeout=90s
+kubectl wait --for=condition=ready ingress --selector=app=backend-ingress --timeout=180s
+kubectl get po -l app=backend-ingress -o wide
 
 sleep 10 # sneaky sleep, instantly curling has issues
 
