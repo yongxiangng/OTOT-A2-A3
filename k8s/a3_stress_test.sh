20e18366ba05597479f681ac2ba24a5df5228ff0
yongxiangng <yongxiangng@gmail.com>
2022-08-26 10:48:34 +0800

    a3.1

--- k8s/a3_stress_test.sh
+++ k8s/a3_stress_test.sh
@@ -2,11 +2,10 @@ kubectl describe hpa --context kind-kind-1
 
 echo curling...
 
-max=100
+max=2000
 for i in `seq 1 $max`
 do
     curl --silent -o /dev/null localhost
 done
-
 
 kubectl describe hpa --context kind-kind-1
